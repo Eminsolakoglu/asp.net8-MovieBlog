2bb92b2563897223bba643528e645bd4f934f293
Emin Solakoğlu <132267025+Eminsolakoglu@users.noreply.github.com>
2025-03-04 13:42:07 +0300

    adminpaneli film güncellemesi

--- MovieApp/MovieApp.Web/Controllers/AdminController.cs
+++ MovieApp/MovieApp.Web/Controllers/AdminController.cs
@@ -19,6 +19,47 @@ public class AdminController : Controller
         return View();
     }
 
+    public IActionResult MovieUpdate(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var entity = _context.Movies
+            .Select(m => new AdminEditMovieViewModel
+            {
+                MovieId = m.MovieId,
+                Title = m.Title,
+                ImageUrl = m.ImageUrl,
+                Description = m.Description
+            }).FirstOrDefault(m=>m.MovieId ==id);
+        if (entity==null)
+        {
+            return NotFound();
+
+        }
+
+        return View(entity);
+    }
+    [HttpPost]
+    public IActionResult MovieUpdate(AdminEditMovieViewModel model)
+    {
+        var entity = _context.Movies.Find(model.MovieId);
+        if (entity == null)
+        {
+            return NotFound();
+        }
+
+        entity.Title = model.Title;
+        entity.Description = model.Description;
+        entity.ImageUrl = model.ImageUrl;
+
+        _context.SaveChanges();
+
+        return RedirectToAction("MovieList");
+    }
+
     public IActionResult MovieList()
     {
         return View(new AdminMoviesViewModel()
--- MovieApp/MovieApp.Web/obj/Debug/net8.0/MovieApp.Web.AssemblyInfo.cs
+++ MovieApp/MovieApp.Web/obj/Debug/net8.0/MovieApp.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6891bffeeaff4c59229b885a6d23fff74dd1b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310c501aad5f7c1aa170f9381561bffbacd25358")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
